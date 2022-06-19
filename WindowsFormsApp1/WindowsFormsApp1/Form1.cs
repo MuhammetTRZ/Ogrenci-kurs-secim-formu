ce035d82aa9515fccc55773cc3608c271674f2b9
MuhammetTRZ <107775151+MuhammetTRZ@users.noreply.github.com>
2022-06-20 00:11:38 +0300

    kurs secım formu
    Class kullanılarak yapılan kurs secım formu

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+       
+        public void güncelle()
+        {
+            
+            
+
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Öğrenci Birsen = new Öğrenci();
+            Birsen.ad = textBox1.Text;
+            Birsen.SoyAd = textBox2.Text;
+            Birsen.Ders = textBox3.Text;
+            Birsen.Düzenle();
+            listBox1.Items.Add(Birsen.Yeni);
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Öğrenci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsFormsApp1
+{
+    internal class Öğrenci
+    {
+        public string ad;
+        public string SoyAd;
+        public string Ders;
+        public string Yeni;
+
+        public void Düzenle()
+        {
+            Yeni = "Öğrencinin adı=" + ad + ", "+ " SoyAdı = " + SoyAd + "İstediği Ders= " + Ders;
+        }
+    }
+}
